6f95c54d6285309cd7c81240140437b2f6cd2dbb
mfelipedev <mfelipe.dev@gmail.com>
2023-12-21 23:03:41 -0300

    MarcoCode

--- app.js
+++ app.js
@@ -10,44 +10,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'my-secret-key', resave: true, saveUninitialized: true }));
 
-// Simulação de banco de dados (objeto para armazenar usuários com senhas criptografadas)
 const usersDb = {};
 
-// Configurar o servidor para servir arquivos estáticos do diretório atual
 app.use(express.static(__dirname));
 
-// Rota inicial
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Rota de registro
 app.get('/registrar', (req, res) => {
   res.sendFile(path.join(__dirname, 'registrar.html'));
 });
 
-// Rota de registro (POST)
 app.post('/registrar', async (req, res) => {
   const { username, password } = req.body;
 
-  // Verificar se o usuário já existe
   if (usersDb[username]) {
     res.status(400).json({ error: 'Usuário já existe. Escolha outro nome de usuário.' });
   } else {
-    // Hash da senha antes de armazenar
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Adicionar o novo usuário à lista
     usersDb[username] = { password: hashedPassword };
     res.status(200).json({ message: `Usuário ${username} registrado com sucesso!` });
   }
 });
 
-// Rota de login
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-  // Lógica de login aqui
   const user = usersDb[username];
 
   if (user && (await bcrypt.compare(password, user.password))) {
@@ -58,17 +48,14 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Rota de dashboard (área protegida)
 app.get('/dashboard', (req, res) => {
-  // Verificar se o usuário está autenticado
   if (req.session.user) {
     res.sendFile(path.join(__dirname, 'usuario.html'));
   } else {
-    res.redirect('/login'); // Redirecionar para o login se não estiver autenticado
+    res.redirect('/login');
   }
 });
 
-// Adicionando mensagem de log
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
--- js/registrar.js
+++ js/registrar.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Aguardar o carregamento completo do DOM antes de atribuir eventos
 
-    // Obter a referência para o botão de registro
     var btnRegistrar = document.getElementById('btnRegistrar');
 
-    // Verificar se o botão foi encontrado
     if (btnRegistrar) {
-        // Adicionar um ouvinte de evento de clique ao botão
         btnRegistrar.addEventListener('click', function () {
             registrar();
         });
@@ -14,13 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 async function registrar() {
-    // Obter os valores dos campos de entrada
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
 
-    // Realizar validações do lado do cliente (adapte conforme necessário)
     if (!username || !password || !email || !phone) {
         document.getElementById('usernot').innerText = 'Preencha todos os campos.';
         document.getElementById('usernot').style.display = 'block';
@@ -32,12 +26,9 @@ async function registrar() {
         return;
     }
 
- // Validar a senha do lado do cliente
  if (!isValidUsername(username)) {
     document.getElementById('usernot').innerText = 'O usuário não pode conter espaços \n ou caracteres especiais.';
     document.getElementById('usernot').style.display = 'block';
-
-    // Aguarde 5 segundos e, em seguida, oculte a mensagem de erro
     setTimeout(function () {
         document.getElementById('usernot').style.display = 'none';
     }, 5000);
@@ -45,12 +36,9 @@ async function registrar() {
     return;
 }
 
-    // Validar a senha do lado do cliente
     if (!isValidPassword(password)) {
         document.getElementById('usernot').innerText = 'A senha deve conter pelo menos 6 caracteres,\n uma letra maiúscula e um caractere especial.';
         document.getElementById('usernot').style.display = 'block';
-
-        // Aguarde 5 segundos e, em seguida, oculte a mensagem de erro
         setTimeout(function () {
             document.getElementById('usernot').style.display = 'none';
         }, 5000);
@@ -58,11 +46,9 @@ async function registrar() {
         return;
     }
 
-    // Validar o telefone para conter apenas números
     if (!isValidPhone(phone)) {
         document.getElementById('usernot').innerText = 'O número de telefone deve conter apenas números.';
         document.getElementById('usernot').style.display = 'block';
-        // Aguarde 5 segundos e, em seguida, oculte a mensagem de erro
         setTimeout(function () {
             document.getElementById('usernot').style.display = 'none';
         }, 5000);
@@ -70,11 +56,9 @@ async function registrar() {
         return;
     }
 
-    // Validar o telefone para conter apenas números
     if (!isValidEmail(email)) {
         document.getElementById('usernot').innerText = 'O e-mail esta incorreto.';
         document.getElementById('usernot').style.display = 'block';
-        // Aguarde 5 segundos e, em seguida, oculte a mensagem de erro
         setTimeout(function () {
             document.getElementById('usernot').style.display = 'none';
         }, 5000);
@@ -82,7 +66,6 @@ async function registrar() {
         return;
     }
 
-    // Chamar o servidor para registrar
     try {
         const response = await fetch("/registrar", {
             method: "POST",
@@ -93,13 +76,11 @@ async function registrar() {
         });
 
         if (response.ok) {
-            // Registro bem-sucedido, redirecionar para a página concluída.
-            console.log('Usuário registrado com sucesso!');
+            //console.log('Usuário registrado com sucesso!');
             window.location.href = 'concluido.html';
         } else {
             const data = await response.json();
             document.getElementById('usernot').style.display = 'block';
-
             setTimeout(function () {
                 document.getElementById('usernot').style.display = 'none';
             }, 5000);
@@ -109,26 +90,20 @@ async function registrar() {
     }
 }
 
-// Função de validação de usuário
+
 function isValidUsername(username) {
-    // Pelo menos 4 caracteres, sem espaço, sem caractere especial
     return /^[a-zA-Z0-9]{4,}$/.test(username);
 }
 
-// Função de validação de senha
+
 function isValidPassword(password) {
-    // Pelo menos 6 caracteres, uma letra maiúscula e um caractere especial
     return /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{6,}$/.test(password);
 }
 
-// Função de validação de telefone
 function isValidPhone(phone) {
-    // Apenas números
     return /^\d+$/.test(phone);
 }
 
-// Função de validação de email
 function isValidEmail(email) {
-    // Validar o formato de e-mail
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
